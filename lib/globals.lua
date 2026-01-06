d5644e822a3c4e380ff0d5ac561e6329adc11b39
Josue Arias <31876967+synthi@users.noreply.github.com>
2026-01-06 15:46:45 -0300

    Update globals.lua

--- lib/globals.lua
+++ lib/globals.lua
@@ -1,62 +1,49 @@
--- Avant_lab_V lib/globals.lua | Version 1019
--- UPDATE: Added grid_mixer_held for interaction
+-- Avant_lab_V lib/globals.lua | Version 1024
+-- UPDATE: Debounce Tables initialized correctly
 
 local Globals = {}
 
 function Globals.new()
   local s = {}
   
-  -- [DEFENSIVE] Visual Data Defaults & Tables
   s.loaded = false 
+  s.amp_l = 0.0; s.amp_r = 0.0; s.comp_gr = 0.0 
   
-  s.amp_l = 0.0
-  s.amp_r = 0.0
-  s.comp_gr = 0.0 
-  
-  -- Explicit declaration of ALL array tables
   s.band_levels = {}
   s.bands_gain = {}  
   s.visual_gain = {} 
   s.grid_memory = {}
   s.grid_cache = {} 
   
-  -- Populate arrays
+  -- Debounce & Input State
+  s.grid_debounce = {}
+  s.button_state = {}
+  
   for i=1, 16 do 
-     s.band_levels[i] = 0.0 
-     s.bands_gain[i] = -60  
-     s.visual_gain[i] = -60 
-     s.grid_memory[i] = -60 
-     
+     s.band_levels[i] = 0.0; s.bands_gain[i] = -60; s.visual_gain[i] = -60; s.grid_memory[i] = -60 
      s.grid_cache[i] = {}
-     for y=1, 8 do s.grid_cache[i][y] = -1 end
+     s.grid_debounce[i] = {}
+     s.button_state[i] = {}
+     for y=1, 8 do 
+        s.grid_cache[i][y] = -1 
+        s.grid_debounce[i][y] = 0
+        s.button_state[i][y] = false
+     end
   end
 
-  -- Ring Buffers
-  s.GONIO_LEN = 50
-  s.FILTER_LEN = 40
-  
-  s.gonio_history = {} 
-  for i=1, s.GONIO_LEN do s.gonio_history[i] = {s=0, w=0} end
-  
-  s.filter_history = {}
-  for i=1, s.FILTER_LEN do s.filter_history[i] = {amp=0, phase=0} end
-  
-  s.time_history = {}
-  for i=1, s.FILTER_LEN do s.time_history[i] = {ph=0, r=0, m=0} end
-
+  s.GONIO_LEN = 50; s.FILTER_LEN = 40
+  s.gonio_history = {}; for i=1, s.GONIO_LEN do s.gonio_history[i] = {s=0, w=0} end
+  s.filter_history = {}; for i=1, s.FILTER_LEN do s.filter_history[i] = {amp=0, phase=0} end
+  s.time_history = {}; for i=1, s.FILTER_LEN do s.time_history[i] = {ph=0, r=0, m=0} end
   s.heads = {gonio=1, filter=1, time=1}
   s.str_cache = {}
 
-  -- Standard State
   s.current_page = 1
-  s.k1_held = false       
-  s.mod_shift_16 = false  
+  s.k1_held = false; s.mod_shift_16 = false  
   s.grid_momentary_mode = false
   s.k2_held_tape = false
   s.time_page_focus = "MAIN"
   s.ping_btn_held = false
-  
-  -- [NEW] Interaction state
   s.grid_mixer_held = false
   
   s.tape_library_sel = 1
@@ -64,18 +51,12 @@ function Globals.new()
   s.tape_msg_timers = {0, 0, 0, 0}
   s.file_selector_active = false
   
-  s.main_rec_slots = {}
-  for i=1, 4 do s.main_rec_slots[i] = {data={}, state=0, press_time=0, start_time=0, step=1, duration=0} end
-  s.main_presets_data = {{}, {}, {}, {}}
-  s.main_presets_status = {0, 0, 0, 0}
-  s.main_preset_selected = 0
-  s.main_preset_press_time = {0,0,0,0}
-  s.main_preset_clicks = {0,0,0,0}
+  s.main_rec_slots = {}; for i=1, 4 do s.main_rec_slots[i] = {data={}, state=0, press_time=0, start_time=0, step=1, duration=0} end
+  s.main_presets_data = {{}, {}, {}, {}}; s.main_presets_status = {0, 0, 0, 0}
+  s.main_preset_selected = 0; s.main_preset_press_time = {0,0,0,0}; s.main_preset_clicks = {0,0,0,0}
   s.morph_main_active = false; s.morph_main_slot = nil
   
-  s.track_sel = 1
-  s.mixer_sel = 1 
-  
+  s.track_sel = 1; s.mixer_sel = 1 
   s.tracks = {}
   for i=1, 4 do
     s.tracks[i] = {
@@ -83,32 +64,20 @@ function Globals.new()
       loop_start = 0.0, loop_end = 1.0, speed = 1.0,
       vol = 0.9, pan = 0.0, rec_level = 1.0, overdub = 1.0,
       wow_macro = 0.0, aux_send = 0.0, src_sel = 0,
-      xfade = 0.05,
-      brake_amt = 0.0,
-      press_time_k2 = 0,
-      is_dirty = false,
-      file_path = nil,
+      xfade = 0.05, brake_amt = 0.0, press_time_k2 = 0,
+      is_dirty = false, file_path = nil,
       l_low = 0.0, l_high = 0.0, l_filter = 0.5, l_pan = 0.0, l_width = 1.0
     }
   end
-  s.tape_rec_slots = {}
-  for i=1, 4 do s.tape_rec_slots[i] = {data={}, state=0, press_time=0, start_time=0, step=1, duration=0} end
-  s.tape_presets_data = {{}, {}, {}, {}}
-  s.tape_presets_status = {0, 0, 0, 0}
-  s.tape_preset_selected = 0
-  s.tape_preset_press_time = {0,0,0,0}
-  s.tape_preset_clicks = {0,0,0,0}
+  s.tape_rec_slots = {}; for i=1, 4 do s.tape_rec_slots[i] = {data={}, state=0, press_time=0, start_time=0, step=1, duration=0} end
+  s.tape_presets_data = {{}, {}, {}, {}}; s.tape_presets_status = {0, 0, 0, 0}
+  s.tape_preset_selected = 0; s.tape_preset_press_time = {0,0,0,0}; s.tape_preset_clicks = {0,0,0,0}
   s.morph_tape_active = false; s.morph_tape_slot = nil
   
-  s.morph_fast_mode = false
-  s.saved_morph_time = 2.0
-
+  s.morph_fast_mode = false; s.saved_morph_time = 2.0
   s.fx_memory = {} 
-  s.transport_timers = {0, 0, 0, 0} 
-  s.transport_press_time = {0, 0, 0, 0} 
-  s.rnd_btn_val = 2
-  s.rnd_btn_timer = 0
-  
+  s.transport_timers = {0, 0, 0, 0}; s.transport_press_time = {0, 0, 0, 0} 
+  s.rnd_btn_val = 2; s.rnd_btn_timer = 0
   s.grid_tape_view_override = false
 
   s.saved_fb = 0; s.saved_tape_fb = 0; s.saved_fmix = 1.0
